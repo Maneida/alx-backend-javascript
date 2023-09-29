231ecc011b4536b530d8e42d3bf337cc9cb19667
Michael Amuah <mkdamuah@hotmail.com>
2023-09-29 01:33:19 +0000

    1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+// Returns array of three initialised Classroom instances/objects
+
+import Classroom from "./0-classroom.js";
+
+export default function initializeRooms() {
+  return [
+    new Classroom(19),
+    new Classroom(20),
+    new Classroom(34),
+  ];
+}
