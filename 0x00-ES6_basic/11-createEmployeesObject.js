f2ef12723ae431e474c36171e1c06bb08828dfc3
Michael Amuah <mkdamuah@hotmail.com>
2023-09-29 23:26:07 +0000

    correction

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,3 @@
-//function named createEmployeesObject that receives two arguments
-
 export default function createEmployeesObject(departmentName, employees) {
   const employeesObj = {};
   employeesObj[departmentName] = employees;
--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,8 +1,3 @@
-/*
-function named createReportObject whose parameter, employeesList, is the 
-return value of the previous function createEmployeesObject
-*/
-
 export default function createReportObject(employeesList) {
   return {
     allEmployees: employeesList,
