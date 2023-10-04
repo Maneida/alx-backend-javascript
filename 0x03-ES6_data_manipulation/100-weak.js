bdf7d1294007fb69ad31f192d9d47517dab9c108
Michael Amuah <mkdamuah@hotmail.com>
2023-10-04 23:03:55 +0000

    correction3

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,8 +3,8 @@ const weakMap = new WeakMap();
 const queryAPI = (endpoint) => {
   let total = weakMap.get(endpoint) || 0;
   if (total >= 5) throw new Error("Endpoint load is high");
-  weakMap.set(endpoint, total + 1);
-  return total + 1;
+  weakMap.set(endpoint, total += 1);
+  return total;
 };
 
 export { weakMap, queryAPI };
